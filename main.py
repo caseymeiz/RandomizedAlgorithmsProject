b98cabcffbc6d386d80eaa55c869fb2bbb0de200
Casey Meisenzahl <caseymeiz@gmail.com>
2022-12-07 12:50:33 -0500

    .

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 from util import Node, union
 from itertools import combinations
 from random import shuffle
-import matplotlib.pyplot as plt
+from time import time
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
 def main():
-    avg_m = list()
-    for i in range(2, 100):
-        avg_m.append(sum(algo(i) for _ in range(20))/20)
-    plt.scatter(x=range(len(avg_m)), y=avg_m)
-    plt.show()
+    n = 300
+    avg_m = [0 for _ in range(n)]
+    execution_time = [0 for _ in range(n)]
+    nodes = [i for i in range(2, n+2)]
+    for i in range(n):
+        print(f'\rworking on {i}/{n}', end='')
+        start = time()
+        avg_m[i] = sum(algo(nodes[i]) for _ in range(5))/5
+        execution_time[i] = time() - start
+
+    df = pd.DataFrame({
+        'm': avg_m,
+        'time': execution_time,
+        'n': nodes
+    })
+
+    fig = make_subplots(
+        rows=2, cols=2,
+        subplot_titles=('Nodes vs First m', 'Nodes vs Time', "Nodes / First m",)
+    )
+
+    fig.add_trace(
+        go.Scatter(x=df.n, y=df.m),
+        row=1, col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(x=df.n, y=df.time),
+        row=1, col=2
+    )
+
+    fig.add_trace(
+        go.Scatter(x=df.n, y=df.m/df.n),
+        row=2, col=1
+    )
+    fig.show()
 
 
 def algo(n):
     nodes = [Node() for _ in range(n)]
-    thresh = len(nodes)/2
     sample_space = list(combinations(nodes, 2))
     shuffle(sample_space)
     for i, (u, v) in enumerate(sample_space, 1):
         x = union(u, v)
-        if x.size >= thresh:
+        if x.size >= len(nodes)/2:
             return i
 
 
